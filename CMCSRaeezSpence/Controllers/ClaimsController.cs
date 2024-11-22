69c2fd34ef2fd375e036f93fa8523896e2a7037c
ST10085639 <ST10085639@vcconnect.edu.za>
2024-11-22 23:55:06 +0200

    Part 3POE project Raeez

---
+++ CMCSRaeezSpence/Controllers/ClaimsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CMCSRaeezSpence.Models;
+
+namespace CMCSRaeezSpence.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        private CMCSDatabaseEntities1 db = new CMCSDatabaseEntities1();
+
+        // GET: Claims
+        public ActionResult Index()
+        {
+            return View(db.Claims.ToList());
+        }
+
+        // GET: Claims/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Claim claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(claim);
+        }
+
+        // GET: Claims/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Claims/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClaimID,SubmittedDate,StartDate,EndDate,HoursWorked,HourlyRate,DocumentPath,Status,UserID,TotalAmount")] Claim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Claims.Add(claim);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(claim);
+        }
+
+        // GET: Claims/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Claim claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(claim);
+        }
+
+        // POST: Claims/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClaimID,SubmittedDate,StartDate,EndDate,HoursWorked,HourlyRate,DocumentPath,Status,UserID,TotalAmount")] Claim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(claim).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(claim);
+        }
+
+        // GET: Claims/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Claim claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(claim);
+        }
+
+        // POST: Claims/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Claim claim = db.Claims.Find(id);
+            db.Claims.Remove(claim);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
